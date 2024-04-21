f9c78ba91ddd00025a25faeda133758e8f1768f7
RealBlueSwan <nobrunner@hotmail.com>
2024-04-21 20:53:48 +0200

    dont really know honestly...

---
+++ ex7/task1a.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+int stack[MAX_SIZE];
+int top = -1;
+
+/*initialize Stack*/
+void new() {
+    top = -1;
+}
+
+/*check if stack is empty*/
+int isEmpty() {
+    return top == -1;
+}
+
+/*check if stack is full*/
+int isFull() {
+    return top == MAX_SIZE - 1;
+}
+
+/*pushes the int data on top of a stack*/
+void push(int data) {
+    if (isFull()){
+        printf("Stack overflow");
+        return;
+    }
+    stack[++top] = data;
+}
+
+/*"pop" the topmost element of the stack and return it. If is_empthy == 1 return -1*/
+int pop() {
+    /* check if stack is empty*/
+    if (isEmpty()){  printf("stack overflow"); return -1;  }
+    return stack[top--];
+}
+
+
+int main(){
+    /*test for a*/
+    new();
+    push(1);
+    push(2);
+    printf("%d\n", pop());  // Should print 2
+    printf("%d\n", pop());  // Should print 1
+    printf("%d\n", pop());  // Should print -1
+    return 0;
+}
---
+++ ex7/task1b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*singly linked lists*/
+struct Node {   int data;   struct Node *next; };
+struct Stack {  struct Node *top;   };
+struct Stack* new() {
+    
+}
