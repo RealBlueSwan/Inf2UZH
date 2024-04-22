ac2f413b470a7d4ee4bab30fb860b14a0eac15ba
RealBlueSwan <nobrunner@hotmail.com>
2024-04-22 12:44:37 +0200

    made some changes to ex2, I made them more clean and more oneliners

--- ex2/code template/exp02_task1.c
+++ ex2/code template/exp02_task1.c
@@ -7,28 +7,20 @@
 */
 int isPalindrome(char A[], int i, int j) {
 
-    if (A[i] != A[j]){
-        return 0;
-    }else if (j <= i)
-    {
-        return 1;
-    }else{
-        return isPalindrome(A, i+1, j-1);
-    }
-    
-
+    if (A[i] != A[j]){  return 0;   }
+    else if (j <= i){   return 1;   }
+    else{   return isPalindrome(A, i+1, j-1);   }
 }
 
 int main() {
     char X[] = "1a_b3cD45tgt54Dc3b_a1";
     int len = sizeof(X) / sizeof(X[0]) - 1;
 
-    if (isPalindrome(X, 0, len - 1)) {
+    if (isPalindrome(X, 0, len - 1)) {  
         printf("This is a Palindrome.");
     }
     else {
         printf("This is not a Palindrome.");
     }
-
     return 0;
 }
